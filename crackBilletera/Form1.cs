6d43adcf5d791195b2ec435b9cba7c0040acda62
Amazing Dev <amazingdev.ni@gmail.com>
2021-09-09 13:53:17 -0600

    remove REMOTE_ADDR header...

--- crackBilletera/Form1.cs
+++ crackBilletera/Form1.cs
@@ -192,7 +192,6 @@ namespace crackBilletera
             string token = getToken(ip);
             using(NewWebClient wc = new NewWebClient(cookieContainer))
             {
-                wc.Headers["REMOTE_ADDR"] = ip;
                 wc.Headers["X-Forwarded-For"] = ip;
                 wc.Headers["Referer"] = "https://billeteramovil.grupopromerica.com";
                 wc.Headers["User-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36";
